80136cd150a343c8915d11f0cb7cbf543d2dc22f
shengwei0515 <shengwei199505@gmail.com>
2021-12-28 22:22:25 +0800

    feat: swagger

--- sealbot.host.api/src/app.controller.ts
+++ sealbot.host.api/src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('api/hello')
   getHello(): string {
     return this.appService.getHello();
   }
--- sealbot.host.api/src/main.ts
+++ sealbot.host.api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const builder = new DocumentBuilder()
+    .setTitle('Seal Bot Host Api')
+    .setDescription('Api server for sealbot to handle domain of twitch host')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, builder);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
