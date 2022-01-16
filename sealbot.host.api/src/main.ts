25d644d40724c2569af29b9dc4c533b862a44e70
shengwei0515 <shengwei199505@gmail.com>
2022-01-16 21:34:31 +0800

    feat: add swagger

--- sealbot.host.api/src/main.ts
+++ sealbot.host.api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const builder = new DocumentBuilder()
+    .setTitle('SealBot Host Api')
+    .setDescription('')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, builder);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
